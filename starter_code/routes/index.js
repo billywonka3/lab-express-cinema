033bd41f7d813e9cd582a52d59eb3660dded14f6
Billy Wong <billywonka3@gmail.com>
2019-07-09 07:52:25 -0400

    Add files via upload

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router  = express.Router();
+const Movie    = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next)=>{
+  Movie.find()
+  .then((database)=>{
+    console.log(database)
+    res.render('movies',{moviesList: database})
+  })
+  .catch((err)=>{
+    console.log(err);
+    next(err);
+  })
+})
+
+router.get('/movies/:details/:id', (req, res, next)=>{
+  Movie.findById(req.params.id)
+  .then((singleMovie)=>{
+    console.log(singleMovie)
+      res.render('movies/show', {theMovie: singleMovie})
+  })
+  .catch((err)=>{
+      console.log(err);
+      next(err);
+  })
 });
 
 module.exports = router;
